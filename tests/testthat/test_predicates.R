0da1fa145d393a6b22e2030e7b48aef985a0fb7b
mir-cat <mir.henglin@gmail.com>
2017-10-25 00:45:48 -0400

    MAINT: added tests for predicates and uncategorized utils

--- tests/testthat/test_predicates.R
+++ tests/testthat/test_predicates.R
@@ -1,2 +1,63 @@
 context('Predicates')
 
+
+# is_scalar_numeric -------------------------------------------------------
+
+test_that('is_scalar_numeric works with Ints', {
+    expect_true(is_scalar_numeric(1L))
+    expect_true(is_scalar_numeric(-1L))
+    expect_true(is_scalar_numeric(0L))
+})
+
+test_that('is_scalar_numeric works with doubles', {
+    expect_true(is_scalar_numeric(5))
+    expect_true(is_scalar_numeric(1/5))
+    expect_true(is_scalar_numeric(-1.123))
+    expect_true(is_scalar_numeric(0.10123))
+})
+
+test_that('is_scalar_numeric works with sci notation', {
+    expect_true(is_scalar_numeric(10e1))
+})
+
+test_that('is_scalar_numeric fails with vector Ints', {
+    expect_false(is_scalar_numeric(c(1L, 2L)))
+})
+
+test_that('is_scalar_numeric fails with vector doubles', {
+    expect_false(is_scalar_numeric(c(5, 0, -1.23)))
+})
+
+test_that('is_scalar_numeric fails with character', {
+    expect_false(is_scalar_numeric('1'))
+    expect_false(is_scalar_numeric(c('1', '2')))
+})
+
+
+
+# is_wholenumber ----------------------------------------------------------
+
+
+test_that('is_wholenumber works with Ints', {
+    expect_true(is_wholenumber(1L))
+    expect_true(is_wholenumber(-1L))
+    expect_true(is_wholenumber(0L))
+    expect_true(is_wholenumber(c(-1L, 0L, 1L)))
+})
+
+test_that('is_wholenumber works with doubles', {
+    expect_true(is_wholenumber(1))
+    expect_true(is_wholenumber(-1))
+    expect_true(is_wholenumber(0))
+    expect_true(is_wholenumber(c(-1, 0, 1)))
+})
+
+test_that('is_wholenumber works with sci notation', {
+    expect_true(is_wholenumber(10e1))
+})
+
+test_that('is_wholenumber fails with character', {
+    expect_false(is_wholenumber('1'))
+    expect_false(is_wholenumber(c('1', '2')))
+})
+
---
+++ tests/testthat/test_utils.R
@@ -0,0 +1,56 @@
+context('utils')
+
+test_that('reverse_split works', {
+
+    nums <- as.character(1:26)
+
+    lst <- as.list(letters)
+    names(lst) <- nums
+    lst2 <- as.list(nums)
+    names(lst2) <- letters
+
+    # reverse split conversts numbers to characters
+    expect_equal(reverse_split(lst), lst2)
+
+    # reverse split does not return names in natural order
+    expect_equal(reverse_split(reverse_split(lst)), lst[sort(names(lst))])
+
+    lst3 <- list(a = 1:4, b = c(2,3), d = c(4,5))
+    lst4 <- list('1' = 'a', '2' = c('a', 'b'), '3' = c('a', 'b'), '4' = c('a', 'd'), '5' = 'd')
+
+    expect_equal(reverse_split(lst3), lst4)
+})
+
+
+# dat_to_mat --------------------------------------------------------------
+
+mat <- matrix(rnorm(260), nrow = 26)
+dat <- dplyr::as_data_frame(mat)
+
+colnames(mat) <- LETTERS[1:10]
+rownames(mat) <- letters
+dat$name <- letters
+
+dtm <- dat_to_mat(dat, 'name')
+
+
+test_that('dat_to_mat returns a matrix', {
+    expect_true(is.matrix(dtm))
+})
+
+test_that('dat_to_mat preserves column names', {
+    expect_equal(colnames(dtm), names(dat[names(dat) != 'name']))
+})
+
+test_that('dat_to_mat gives proper rownames', {
+    expect_equal(rownames(dtm), dat$name)
+})
+
+test_that('dat_to_mat preserves data', {
+    expect_equivalent(dtm, mat)
+})
+
+test_that('dat_to_mat works with all tidyeval forms', {
+    expect_equal(dat_to_mat(dat, name), dat_to_mat(dat, 'name'))
+    expect_equal(dat_to_mat(dat, name), dat_to_mat(dat, 11))
+})
