2e7ca951013b89cff460fb6ac5752226ea2f00e8
mir-cat <mir.henglin@gmail.com>
2017-12-19 18:24:22 -0500

    Refactored cross_ for api consistency

--- R/cross.R
+++ R/cross.R
@@ -1,15 +1,16 @@
-#' Crosses some vectors in a list, splicing other elements into each cross Useful for generating lists of substitutions
+#' Crosses some vectors in a list, splicing other elements into each cross
+#' Useful for generating lists of substitutions
 #'
 #' @param .l list of vectors
 #' @param .at A character vector of names or a numeric vector of positions
-#'
+#' @param ... arguments passed to \code{\link[purrr]{cross}}
 #' @return list
 #' @export
 #'
-cross_some <- function(.l, .at) {
+cross_at <- function(.l, .at, ...) {
     sel <- inv_which(.l, .at)
 
-    purrr::cross(.l[sel]) %>%
+    purrr::cross(.l[sel], ...) %>%
         purrr::map(~c(., .l[!sel]))
 }
 
